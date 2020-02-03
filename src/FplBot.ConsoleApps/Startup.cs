b03b3701761be64a3cdb34e62545c19151690f83
Korsneso <johnkors@gmail.com>
2020-02-03 20:41:19 +0100

    Expose version in ping endpoint

--- src/FplBot.ConsoleApps/Startup.cs
+++ src/FplBot.ConsoleApps/Startup.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace FplBot.ConsoleApps
 {
@@ -8,7 +10,13 @@ namespace FplBot.ConsoleApps
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.Run(async context => { await context.Response.WriteAsync("Hello, World!"); });
+            var info = new
+            {
+                Version = Assembly.GetEntryAssembly().GetName().Version.ToString(),
+                FileVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>().Version,
+                InformationalVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            };
+            app.Run(async context => { await context.Response.WriteAsync(JsonConvert.SerializeObject(info)); });
         }
     }
 }
