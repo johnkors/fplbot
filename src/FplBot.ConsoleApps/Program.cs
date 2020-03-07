be137191643511b476c1993b46cd378c16c71b2a
Korsneso <johnkors@gmail.com>
2020-03-07 15:38:32 +0100

    rebase fix

--- src/FplBot.ConsoleApps/Program.cs
+++ src/FplBot.ConsoleApps/Program.cs
@@ -1,12 +1,10 @@
 using System;
-using Microsoft.AspNetCore;
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Events;
 using Slackbot.Net.Abstractions.Hosting;
 using Slackbot.Net.Extensions.Publishers.Logger;
 using Slackbot.Net.Extensions.Publishers.Slack;
